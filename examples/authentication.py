aedd0e1d4f41e295c4af02562ca133748c586604
Paul Tindall <paul.tindall@rxfoundry.com>
2025-10-06 22:41:58 -0500

    SWFT-338: add patients example

--- examples/authentication.py
+++ examples/authentication.py
@@ -1,35 +1,30 @@
 import rxfoundry.clients.swifty_oauth_api
+import argparse
 from rxfoundry.clients.swifty_oauth_api import TokenResponse, UserInfoResponse
 from rxfoundry.clients.swifty_oauth_api.rest import ApiException
 
 
-def get_auth_tokens(username: str, password: str) -> TokenResponse | None:
-    configuration = rxfoundry.clients.swifty_oauth_api.Configuration(
-        host = "https://bootstrap.swiftyrx.dev"
-    )
+def get_auth_tokens(host: str, username: str, password: str) -> TokenResponse | None:
+    configuration = rxfoundry.clients.swifty_oauth_api.Configuration(host=host)
     with rxfoundry.clients.swifty_oauth_api.ApiClient(configuration) as api_client:
         api_instance = rxfoundry.clients.swifty_oauth_api.OAuthApi(api_client)
         get_token_request = rxfoundry.clients.swifty_oauth_api.GetTokenRequest(
-            grant_type="password",
-            username=username,
-            password=password
+            grant_type="password", username=username, password=password
         )
 
         try:
-            api_response = api_instance.get_token(
-                get_token_request
-            )
+            api_response = api_instance.get_token(get_token_request)
             return api_response
         except ApiException as e:
             print("Exception when calling DefaultApi->get_token: %s\n" % e)
 
-def get_user_info(token) -> UserInfoResponse:
+
+def get_user_info(token) -> UserInfoResponse | None:
     configuration = rxfoundry.clients.swifty_oauth_api.Configuration(
-        host = "https://bootstrap.swiftyrx.dev"
+        host="https://bootstrap.swiftyrx.dev"
     )
     with rxfoundry.clients.swifty_oauth_api.ApiClient(configuration) as api_client:
         api_instance = rxfoundry.clients.swifty_oauth_api.OAuthApi(api_client)
-
 
         try:
             api_response = api_instance.get_user_info(
@@ -39,8 +34,15 @@ def get_user_info(token) -> UserInfoResponse:
         except ApiException as e:
             print("Exception when calling DefaultApi->get_user_info: %s\n" % e)
 
+
 if __name__ == "__main__":
-    token_response = get_auth_tokens("paul.tindall@rxfoundry.com", "B3@mM3UpSc0tty")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host", help="The host to authenticate against.")
+    parser.add_argument("username", help="The username for authentication.")
+    parser.add_argument("password", help="The password for authentication.")
+    args = parser.parse_args()
+
+    token_response = get_auth_tokens(args.host, args.username, args.password)
     print(token_response)
     user_info = get_user_info(token_response.access_token)
     print(user_info)
