4f29e93a51be8cf3b5b4893768e0fa6ec19d0071
Paul Tindall <paul.tindall@rxfoundry.com>
2025-10-08 11:02:53 -0500

    SWFT-338: final updates

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
-To get started:
+### INSTALLATION
+To install the dependencies, run:
+```
+pip install poetry
+poetry install
+```
 
-`pip install poetry`
+### UPDATE
+To get the latest SDK
+```
+poetry update
+```
 
-`poetry install`
+### FILES
+#### authentication.py
+Demonstrates how to get an access token needed by other APIs.  Also shows how to get an OAuth2 style userinfo object for a given token.
+
+#### async.py
+Demonstrates how to use the async api to add/update patient data, patient address data and patient insurance data.
+
+#### patients.py
+
+Demonstrates how to use the patients api to search for and retrieve patient data.
+
+#### prescriptions.py
+
+Demonstrates how to use the prescriptions api to search for and retrieve prescription data.
+
 
--- examples/async.py
+++ examples/async.py
@@ -20,11 +20,42 @@ def add_patient_data(host: str, token: str):
                     PatientData(
                         first_name="John",
                         last_name="Doe",
-                        gender_assigned_at_birth="F",
+                        gender_assigned_at_birth="M",
                         date_of_birth="1970-04-25",
                         email="ptindall+john+doe@gmail.com",
                         object_type="patient",
                         primary_phone="512 605-9456"
+                    )
+                )
+            )
+            api_response = api_instance.create_patient_activity(
+                patient_activity_notification=patient_activity_notification
+            )
+            return api_response
+        except ApiException as e:
+            print("Exception when calling AsyncApi->add_patient_data: %s\n" % e)
+
+def update_patient_data(host: str, token: str):
+    configuration = rxfoundry.clients.swifty_api.Configuration(
+        host=f"{host}/api", access_token=token
+    )
+    with rxfoundry.clients.swifty_api.ApiClient(configuration) as api_client:
+        api_instance = rxfoundry.clients.swifty_api.AsyncApi(api_client)
+        try:
+            patient_activity_notification = PatientActivityNotification(
+                object_type="patient",
+                external_patient_id="7654321",
+                external_system_slug="CPD-FRONTEND",
+                action = "updated",
+                activity_data=PatientActivityNotificationActivityData(
+                    PatientData(
+                        first_name="Jane",
+                        last_name="Sanchez",
+                        gender_assigned_at_birth="F",
+                        date_of_birth="1970-04-25",
+                        email="ptindall+jane+doe@gmail.com",
+                        object_type="patient",
+                        primary_phone="512 349-0404"
                     )
                 )
             )
@@ -65,8 +96,98 @@ def add_patient_address_data(host: str, token: str):
         except ApiException as e:
             print("Exception when calling AsyncApi->add_patient_data: %s\n" % e)
 
+def update_patient_address_data(host: str, token: str):
+    configuration = rxfoundry.clients.swifty_api.Configuration(
+        host=f"{host}/api", access_token=token
+    )
+    with rxfoundry.clients.swifty_api.ApiClient(configuration) as api_client:
+        api_instance = rxfoundry.clients.swifty_api.AsyncApi(api_client)
+        try:
+            patient_activity_notification = PatientActivityNotification(
+                object_type="address",
+                external_patient_id="7654322",
+                external_system_slug="CPD-FRONTEND",
+                action = "updated",
+                activity_data=PatientActivityNotificationActivityData(
+                    PatientAddressData(
+                        address_line_one="125 Main Street",
+                        city="Boston",
+                        state_province="MA",
+                        postal_code="02116",
+                        object_type="address",
+                        country_code="US",
+                    )
+                )
+            )
+            api_response = api_instance.create_patient_activity(
+                patient_activity_notification=patient_activity_notification
+            )
+            return api_response
+        except ApiException as e:
+            print("Exception when calling AsyncApi->add_patient_data: %s\n" % e)
+
+def add_patient_insurance_data(host: str, token: str):
+    configuration = rxfoundry.clients.swifty_api.Configuration(
+        host=f"{host}/api", access_token=token
+    )
+    with rxfoundry.clients.swifty_api.ApiClient(configuration) as api_client:
+        api_instance = rxfoundry.clients.swifty_api.AsyncApi(api_client)
+        try:
+            patient_activity_notification = PatientActivityNotification(
+                object_type="insurance",
+                external_patient_id="7654321",
+                external_system_slug="CPD-FRONTEND",
+                action = "created",
+                activity_data=PatientActivityNotificationActivityData(
+                    PatientInsuranceData(
+                        member_id="123456789",
+                        rx_group="RX1234",
+                        rx_bin="012345",
+                        rx_pcn="1234567",
+                        object_type="insurance",
+                    )
+                )
+            )
+            api_response = api_instance.create_patient_activity(
+                patient_activity_notification=patient_activity_notification
+            )
+            return api_response
+        except ApiException as e:
+            print("Exception when calling AsyncApi->add_patient_insurance_data: %s\n" % e)
+
+
+def update_patient_insurance_data(host: str, token: str):
+    configuration = rxfoundry.clients.swifty_api.Configuration(
+        host=f"{host}/api", access_token=token
+    )
+    with rxfoundry.clients.swifty_api.ApiClient(configuration) as api_client:
+        api_instance = rxfoundry.clients.swifty_api.AsyncApi(api_client)
+        try:
+            patient_activity_notification = PatientActivityNotification(
+                object_type="insurance",
+                external_patient_id="7654321",
+                external_system_slug="CPD-FRONTEND",
+                action = "updated",
+                activity_data=PatientActivityNotificationActivityData(
+                    PatientInsuranceData(
+                        member_id="123456789",
+                        rx_group="RX1234",
+                        rx_bin="543210",
+                        rx_pcn="754321",
+                        object_type="insurance",
+                    )
+                )
+            )
+            api_response = api_instance.create_patient_activity(
+                patient_activity_notification=patient_activity_notification
+            )
+            return api_response
+        except ApiException as e:
+            print("Exception when calling AsyncApi->add_patient_insurance_data: %s\n" % e)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+
     parser.add_argument("host", help="The host to authenticate against.")
     parser.add_argument("username", help="The username for authentication.")
     parser.add_argument("password", help="The password for authentication.")
@@ -78,5 +199,24 @@ if __name__ == "__main__":
     # add_patient_data_response = add_patient_data(args.host, token_response.access_token)
     # print(add_patient_data_response)
 
-    add_patient_address_data_response = add_patient_address_data(args.host, token_response.access_token)
-    print(add_patient_address_data_response)
+    # add_patient_address_data_response = add_patient_address_data(args.host, token_response.access_token)
+    # print(add_patient_address_data_response)
+
+    # add_patient_insurance_data_response = add_patient_insurance_data(args.host, token_response.access_token)
+    # print(add_patient_insurance_data_response)
+
+    # update_patient_data_response = update_patient_data(args.host, token_response.access_token)
+    # print(update_patient_data_response)
+
+    # update_patient_address_data_response = update_patient_address_data(args.host, token_response.access_token)
+    # print(update_patient_address_data_response)
+
+    # update_patient_insurance_data_response = update_patient_insurance_data(args.host, token_response.access_token)
+    # print(update_patient_insurance_data_response)
+
+
+
+
+
+
+
